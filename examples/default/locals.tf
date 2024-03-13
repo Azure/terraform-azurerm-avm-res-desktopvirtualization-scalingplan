9d6537a874800d9e1f9245e4387df0095617df10
Jen Sheerin <jensheerin@microsoft.com>
2024-03-13 01:10:24 -0400

    modify regions

--- examples/default/locals.tf
+++ examples/default/locals.tf
@@ -1,16 +1,23 @@
 # We pick a random region from this list.
 locals {
   azure_regions = [
+    "ukwest",
+    "uksouth",
+    "centralindia",
+    "australiaeast",
+    "canadacentral",
+    "canadaeast",
+    "japaneast",
     "westeurope",
     "northeurope",
     "eastus",
     "eastus2",
     "westus",
     "westus2",
+    "westus3",
     "southcentralus",
     "northcentralus",
     "centralus",
-    "eastasia",
-    "southeastasia",
+    "westcentralus"
   ]
 }
